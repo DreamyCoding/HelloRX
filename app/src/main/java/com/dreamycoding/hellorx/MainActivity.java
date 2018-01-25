08df6fc7b11ef51850769f0f80b8d8a3ddee8b2e
JuyelRana <juyelcse11th@gmail.com>
2018-01-25 18:09:39 +0600

    Updated

--- app/src/main/java/com/dreamycoding/hellorx/MainActivity.java
+++ app/src/main/java/com/dreamycoding/hellorx/MainActivity.java
@@ -2,12 +2,31 @@ package com.dreamycoding.hellorx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtView)
+    TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        Observable.just("Hello! Please use this app").subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                txtView.setText(s);
+            }
+        });
     }
 }
